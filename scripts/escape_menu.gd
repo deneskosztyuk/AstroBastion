01e86e8d8951f8ceb94564148041fc86ccb6f6fd
Denes Kosztyuk <denes.kosztyuk@gmail.com>
2024-05-28 18:21:41 +0100

    added pause menu, ui updates

---
+++ scripts/escape_menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+var is_paused = false
+
+func _ready():
+	# Connect the button signals to the corresponding functions
+	$Panel/VBoxContainer/ResumeButton.pressed.connect(_on_ResumeButton_pressed)
+	$Panel/VBoxContainer/OptionsButton.pressed.connect(_on_OptionsButton_pressed)
+	$Panel/VBoxContainer/ExitButton.pressed.connect(_on_ExitButton_pressed)
+	
+	# Hide the EscapeMenu initially
+	visible = false
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		is_paused = !is_paused
+		get_tree().paused = is_paused
+		visible = is_paused
+		print("Esc key pressed")
+
+func _on_ResumeButton_pressed():
+	print("Resume button pressed")
+	is_paused = false
+	get_tree().paused = false
+	visible = false
+
+func _on_OptionsButton_pressed():
+	print("Options button pressed")
+
+func _on_ExitButton_pressed():
+	print("Exit button pressed")
+	get_tree().quit()
