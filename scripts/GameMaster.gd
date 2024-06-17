f03594eb4fbd2d33a44082c602a04a4bf2d2fb20
Denes Kosztyuk <denes.kosztyuk@gmail.com>
2024-06-17 18:44:38 +0100

    added wave mechanics, wave label

--- scripts/GameMaster.gd
+++ scripts/GameMaster.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var enemy_scene: PackedScene = preload("res://Scenes/enemy_t_1.tscn")
 @export var TurretT1: PackedScene = preload("res://Scenes/TurretT1.tscn")
 @onready var spawn_timer = $SpawnTimer
+@onready var break_timer = $"../BreakTimer"
 @export var UI: PackedScene = preload("res://Scenes/ui.tscn")  # Ensure this matches the exact case of your file
 @onready var background_music = $"../BackgroundMusic"
 @onready var siren = $"../Siren"
@@ -16,6 +17,8 @@ var resource = 50
 var turret_cost = 50
 var kill_count = 0
 var elapsed_time = 0
+var wave_number = 1
+var enemies_remaining = 0
 var timer = Timer.new()
 
 var spawn_points = []
@@ -30,8 +33,13 @@ func _ready():
 			spawn_points.append(spawn_point)
 	
 	spawn_timer.connect("timeout", Callable(self, "_on_spawn_timer_timeout"))
-	spawn_timer.wait_time = 5.0 # Set the time interval for spawning enemies
+	spawn_timer.wait_time = 1.0 # Set the time interval for spawning enemies
 	spawn_timer.start()
+	spawn_timer.stop()
+
+	break_timer.connect("timeout", Callable(self, "_on_break_timer_timeout"))
+	break_timer.wait_time = 15.0 # 15 seconds break between waves
+	break_timer.start()
 
 	# Instantiate the UI scene and add it to the main scene
 	ui_instance = UI.instantiate()
@@ -42,6 +50,7 @@ func _ready():
 	update_resource_label()
 	update_kill_count_label()
 	update_time_remaining_label()
+	update_wave_label()  # Initialize the wave label with the current wave
 
 	# Connect the turret_button_pressed signal to the _on_turret_button_pressed function
 	ui_instance.connect("turret_button_pressed", Callable(self, "_on_turret_button_pressed"))
@@ -78,7 +87,12 @@ func _unhandled_input(event):
 			turret_instance = null
 
 func _on_spawn_timer_timeout():
-	spawn_enemy()
+	if enemies_remaining > 0:
+		spawn_enemy()
+		enemies_remaining -= 1
+	else:
+		spawn_timer.stop()
+		break_timer.start()
 
 func spawn_enemy():
 	if spawn_points.size() == 0:
@@ -89,7 +103,16 @@ func spawn_enemy():
 	enemy_instance.global_position = random_spawn_point.global_position
 	add_child(enemy_instance)
 	enemy_instance.look_at(spaceship.global_position) # Make the enemy face the spaceship
-	
+
+func _on_break_timer_timeout():
+	start_next_wave()
+
+func start_next_wave():
+	wave_number += 1
+	enemies_remaining = wave_number  # Spawn 1 more enemy than the previous wave
+	spawn_timer.start()
+	update_wave_label()
+
 func _on_turret_button_pressed(turret_type):
 	if turret_type == "TurretT1":
 		if turret_count_t1 < max_turrets_t1 and resource >= turret_cost:
@@ -115,18 +138,26 @@ func update_resource_label():
 		push_error("Resource label not found!")
 
 func update_kill_count_label():
-	var kill_count_label = ui_instance.get_node("Camera2D/Control/KillCount/KillCountIcon/KillCountLabel")
+	var kill_count_label = ui_instance.get_node("Camera2D/Control/KillCount/KillCountLabel")
 	if kill_count_label:
 		kill_count_label.text = "Kill Count: " + str(kill_count)
 	else:
 		push_error("Kill count label not found!")
 
 func update_time_remaining_label():
-	var time_remaining_label = ui_instance.get_node("Camera2D/Control/TimeRemaining/TimeRemainingObj/TimeRemainingLabel")
+	var time_remaining_label = ui_instance.get_node("Camera2D/Control/TimeRemaining/TimeRemainingLabel")
 	if time_remaining_label:
 		time_remaining_label.text = "Survived for: " + str(elapsed_time) + " seconds."
 	else:
 		push_error("Time remaining label not found!")
+
+func update_wave_label():
+	var wave_label = ui_instance.get_node("Camera2D/Control/Wave/WaveLabel")
+	if wave_label:
+		print("Wave label found, updating text.")
+		wave_label.text = "Wave: " + str(wave_number)
+	else:
+		push_error("Wave label not found!")
 
 func add_resource(amount):
 	resource += amount
--- scripts/TurretT1.gd
+++ scripts/TurretT1.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 @export var Projectile: PackedScene
 @onready var target_lock_zone = $TargetLockZone
+@onready var front_reference = $FrontReference
 @export var _rotation_speed = 3.0
-@export var shooting_interval = 0.4  # Adjust the shooting interval as needed
+@export var shooting_interval = 0.4  
 
 var is_shooting = false
 var target = null
@@ -15,17 +16,20 @@ func _ready():
 
 func _physics_process(delta):
 	if target and target.is_in_group("enemies"):
-		var direction = (target.global_position - global_position).normalized()
-		var target_angle = direction.angle()
-		rotation = lerp_angle(rotation, target_angle, delta * _rotation_speed)
+		var direction_to_target = (target.global_position - global_position).normalized()
+		var direction_to_front = (front_reference.global_position - global_position).normalized()
+		var angle_to_target = direction_to_front.angle_to(direction_to_target)
+		var rotation_direction = 1 if angle_to_target > 0 else -1
+		rotation += rotation_direction * min(abs(angle_to_target), _rotation_speed * delta)
+
 
 func shoot_at(target_enemy):
 	if Projectile and target_enemy.is_in_group("enemies"):
 		var projectile = Projectile.instantiate()
 		get_parent().add_child(projectile)
 		projectile.global_position = $ProjectileSpawnPoint.global_position
-		var direction = (target_enemy.global_position - $ProjectileSpawnPoint.global_position).normalized()
-		projectile.launch(direction)
+		var projectile_direction = (front_reference.global_position - global_position).normalized()
+		projectile.launch(projectile_direction)
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("enemies"):
